f9d22422e922b26eedeef1aa252185ade5d539d4
Justin <Justin@grandcircus.co>
2023-01-25 15:40:43 -0500

    Created MathTwo class added methods

---
+++ Group Project/MathTwo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project
+{
+    public class MathTwo
+    {
+        public static int Adder(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subber(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multipler(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Divider(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
